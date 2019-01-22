5bf9edd1a033720c27ef4a56900649a4bde62835
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2019-01-21 23:34:26 -0500

    Polymer 3 Tutorial Step 2

--- icon-toggle.js
+++ icon-toggle.js
@@ -4,18 +4,21 @@ import '@polymer/iron-icon/iron-icon.js';
 class IconToggle extends PolymerElement {
   static get template() {
     return html`
-      <style>
-        /* shadow DOM styles go here */
-        span {
-          color: blue;
-        }
+          <style>
         :host {
           display: inline-block;
+        }
+        iron-icon {
+          fill: rgba(0,0,0,0);
+          stroke: currentcolor;
+        }
+        :host([pressed]) iron-icon {
+          fill: currentcolor;
         }
       </style>
   
       <!-- shadow DOM goes here -->
-      <span>Not much here yet.</span>
+      <iron-icon icon="polymer"></iron-icon>
     `;
   }
   constructor() {
